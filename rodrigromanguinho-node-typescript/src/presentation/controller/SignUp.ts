31117e5ea8275edd8fbf95020e81a5653e3b899b
Nash Junior <nashjuniorjj@gmail.com>
2020-10-01 09:43:40 -0300

    feat: ensure SignUpController return an erro if no email isProvided

--- rodrigromanguinho-node-typescript/src/presentation/controller/SignUp.ts
+++ rodrigromanguinho-node-typescript/src/presentation/controller/SignUp.ts
@@ -1,8 +1,14 @@
 export default class SignUpController {
   handle(httpRequest: any): any {
-    return {
-      statusCode: 400,
-      body: new Error(`Missing param: name`),
-    };
+    const httpResponse: any = {};
+    if (!httpRequest.body.name) {
+      httpResponse.statusCode = 400;
+      httpResponse.body = new Error(`Missing param: name`);
+    }
+    if (!httpRequest.body.email) {
+      httpResponse.statusCode = 400;
+      httpResponse.body = new Error(`Missing param: email`);
+    }
+    return httpResponse;
   }
 }

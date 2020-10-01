8667e705b45ac4f2709906e8e2a2a17fceb8fcb0
Nash Junior <nashjuniorjj@gmail.com>
2020-10-01 11:57:18 -0300

    refactor: add customized error for missging params

--- rodrigromanguinho-node-typescript/src/presentation/controller/SignUp.ts
+++ rodrigromanguinho-node-typescript/src/presentation/controller/SignUp.ts
@@ -1,15 +1,16 @@
 import { HttpRequest, HttpResponse } from '../protocols/Http';
+import MissingParamError from '../errors/MissingParamError';
 
 export default class SignUpController {
   handle(httpRequest: HttpRequest): HttpResponse {
     const httpResponse = {} as HttpResponse;
     if (!httpRequest.body.name) {
       httpResponse.statusCode = 400;
-      httpResponse.body = new Error(`Missing param: name`);
+      httpResponse.body = new MissingParamError(`name`);
     }
     if (!httpRequest.body.email) {
       httpResponse.statusCode = 400;
-      httpResponse.body = new Error(`Missing param: email`);
+      httpResponse.body = new MissingParamError(`email`);
     }
     return httpResponse;
   }

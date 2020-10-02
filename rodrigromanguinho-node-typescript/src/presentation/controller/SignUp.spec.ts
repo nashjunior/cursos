f85b2d6fa1674588696bdf047f66135725b31251
Nash Junior <nashjuniorjj@gmail.com>
2020-10-02 11:54:07 -0300

    test: ensure SignUpController return 400 if no passwordConfirmation is provided

--- rodrigromanguinho-node-typescript/src/presentation/controller/SignUp.spec.ts
+++ rodrigromanguinho-node-typescript/src/presentation/controller/SignUp.spec.ts
@@ -41,4 +41,19 @@ describe(`SignUp controller`, () => {
     const httpResponse: HttpResponse = sut.handle(httpRequest);
     expect(httpResponse.body).toEqual(new MissingParamError(`password`));
   });
+
+  test(`Should return 400 if no passwordConfirmation is provided`, () => {
+    const sut = new SignUpController();
+    const httpRequest = {
+      body: {
+        email: `any_email@mail.com`,
+        name: `any_email`,
+        password: `any_password`,
+      },
+    };
+    const httpResponse: HttpResponse = sut.handle(httpRequest);
+    expect(httpResponse.body).toEqual(
+      new MissingParamError(`passwordConfirmation`),
+    );
+  });
 });

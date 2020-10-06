4ebe28b0d648000fe445993c3b1173f9a395de6c
Nash Junior <nashjuniorjj@gmail.com>
2020-10-06 14:09:37 -0300

    feat: ensure DbAddAccount calls Encrypter with correct password

---
+++ rodrigromanguinho-node-typescript/src/data/useCase/addAccount/DbAddAccount.ts
@@ -0,0 +1,20 @@
+import { resolve } from 'path';
+import { AccountModel } from '../../../domain/models/Account';
+import {
+  AddAccount,
+  AddAccountModel,
+} from '../../../domain/useCases/AddAccount';
+import { Encrypter } from '../../protocols/Encrypter';
+
+export default class DbAddAccount implements AddAccount {
+  private readonly encrypter: Encrypter;
+
+  constructor(encrypter: Encrypter) {
+    this.encrypter = encrypter;
+  }
+
+  async add(account: AddAccountModel): Promise<AccountModel> {
+    await this.encrypter.encrypt(account.password);
+    return new Promise(resolve => resolve(null));
+  }
+}

83c5dad6540046029631ad1cf5aa80154b5b1605
Nash Junior <nashjuniorjj@gmail.com>
2020-10-07 12:30:59 -0300

    feat: ensure cors is enabled

---
+++ rodrigromanguinho-node-typescript/src/main/middlewares/Cors.ts
@@ -0,0 +1,10 @@
+import { Request, Response, NextFunction } from 'express';
+
+const cors = (req: Request, res: Response, next: NextFunction) => {
+  res.set(`access-control-allow-origin`, `*`);
+  res.set(`access-control-allow-methods`, `*`);
+  res.set(`access-control-allow-headers`, `*`);
+  next();
+};
+
+export default cors;

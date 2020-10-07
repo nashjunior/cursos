d35846c748645186dc555a5deaf886826361788a
Nash Junior <nashjuniorjj@gmail.com>
2020-10-07 12:42:58 -0300

    refactor: cors file return any

--- rodrigromanguinho-node-typescript/src/main/middlewares/Cors.ts
+++ rodrigromanguinho-node-typescript/src/main/middlewares/Cors.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 
-const cors = (req: Request, res: Response, next: NextFunction) => {
+const cors = (req: Request, res: Response, next: NextFunction): any => {
   res.set(`access-control-allow-origin`, `*`);
   res.set(`access-control-allow-methods`, `*`);
   res.set(`access-control-allow-headers`, `*`);

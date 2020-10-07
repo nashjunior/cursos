44dad0b9493b1167e3edaeb224b177dbcb9af490
Nash Junior <nashjuniorjj@gmail.com>
2020-10-07 12:51:43 -0300

    refactor: group middlewares in a single file

--- rodrigromanguinho-node-typescript/src/main/confg/middlewares.ts
+++ rodrigromanguinho-node-typescript/src/main/confg/middlewares.ts
@@ -1,7 +1,6 @@
 import { Express } from 'express';
-import bodyParser from '../middlewares/BodyParser';
-import cors from '../middlewares/Cors';
-import contentType from '../middlewares/ContentType';
+
+import { bodyParser, cors, contentType } from '../middlewares';
 
 export default (app: Express): void => {
   app.use(bodyParser);
---
+++ rodrigromanguinho-node-typescript/src/main/middlewares/index.ts
@@ -0,0 +1,3 @@
+export { default as bodyParser } from './BodyParser';
+export { default as contentType } from './ContentType';
+export { default as cors } from './Cors';
